5e27e0b2f39e621f33015fdf1e7ba92ad5afb79b
AdrianaSche <schepers2008@hotmail.de>
2021-09-04 19:40:23 +0200

    add security-conf with inMemory-authentication username and password

---
+++ backend/src/main/java/de/adrianaschepers/shishabuddies/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package de.adrianaschepers.shishabuddies.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("username")
+                .password("password")
+                .roles("user");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                //alle Routen sind abgesichert
+                .antMatchers("/**").authenticated()
+                .and().formLogin()
+                .and().httpBasic();
+    }
+}
